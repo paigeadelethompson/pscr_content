dff0b7e798ca9e9991dfe222923b1ca7c3f89853
erratic <erratic@shells.yourstruly.sx>
2018-08-12 08:08:29 +0000

    updates to javascript class

--- extensions/pscr_content/html/javascript.php
+++ extensions/pscr_content/html/javascript.php
@@ -11,14 +11,27 @@ namespace pscr\extensions\pscr_content\html;
 
 class javascript extends script
 {
-    function __construct($arguments)
+    function __construct($arguments = null)
     {
         parent::__construct();
-        $this->src = $arguments[0];
+        if($arguments != null)
+            $this->src = $arguments[0];
     }
 
     public function __toString()
     {
         return "script";
+    }
+
+    public function integrity($integ)
+    {
+        $this->integrity = $integ;
+        return $this;
+    }
+    
+    public function cross_origin($origin)
+    {
+        $this->crossorigin = $origin;
+        return $this;
     }
 }
