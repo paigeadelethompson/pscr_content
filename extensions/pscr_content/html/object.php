7d2c326edb9e34c9036584744b361dd02d92b12f
Paige Thompson <paigeadele@gmail.com>
2022-08-01 00:58:01 -0700

    object.php not implemented atn

--- extensions/pscr_content/html/object.php
+++ extensions/pscr_content/html/object.php
@@ -7,9 +7,14 @@
  */
 
 namespace pscr\extensions\pscr_content\html;
+
 use pscr\extensions\pscr_content\model\html_tag;
+use pscr\lib\exceptions\not_implemented_exception;
 
 class objekt extends html_tag
 {
-
+  public function __toString()
+  {
+    throw new not_implemented_exception();
+  }
 }
